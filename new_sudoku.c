b96ce450d682030bd8087516e3021fe7925cab45
Haroun Habeeb <haroun7@gmail.com>
2016-04-04 17:58:40 +0530

    Drowning in the c

--- new_sudoku.c
+++ new_sudoku.c
@@ -4,10 +4,12 @@
 #include "sudoku.h"
 // #define SIZE 9
 #define BOARD_SIZE (SIZE*SIZE + 1)
+
 #define NO_SOLN 2
 #define PARTIAL_SOLN 1
 #define SOLVED 0
-#define MAX_INT 2*SIZE
+
+#define INITIAL_POOL_SIZE 5
 
 struct cell_t; // r, c, allowed, n_allowed
 typedef struct cell_t cell_t;
@@ -18,7 +20,10 @@ typedef struct cell_t cell_t;
 	void remove_allowed(cell_t* this, int val);
 	int set_value(cell_t*, int);
 	int cell_destructor(cell_t*);
-
+	cell_t* deepcopy_board(cell_t* this);
+	
+	void print_cell(cell_t*);
+	void print_board(cell_t*);
 struct pool_t; // Thread's access this... array of board's, FIFO/LIFO/PQ behaviour to be defined.
 	struct pool_node_t; // private struct for pool_t really.
 typedef struct pool_t pool_t;
@@ -27,11 +32,15 @@ typedef struct pool_t pool_t;
 	// Functions of : pool_t
 	int is_empty(pool_t* this);
 	void free_pool(pool_t* this);
-	void pushf(pool_t*, cell_t*);
-	void pushb(pool_t*, cell_t*);
+	cell_t* pushf(pool_t*, cell_t*);
+	cell_t* pushb(pool_t*, cell_t*);
 	cell_t* popf(pool_t*);
 	cell_t* popb(pool_t*);
 
+	#define THRESHOLD_FOR_BRANCH 10
+	int branch(pool_t* workpool, cell_t* board, int nbranches);
+	int dfs(cell_t* board);
+	int heuristic_solve(cell_t* board);	
 struct cell_t {
 	int row;
 	int col;
@@ -44,6 +53,8 @@ struct cell_t {
 	void cell_init(cell_t* this) {
 		this->list = (int*) malloc( (SIZE+1)*sizeof(int) ); // idx0 corresponds to zilch.
 		this->base_array = (int*) malloc( (SIZE+1)*sizeof(int) ); // idx0 is useless... value 0 means not allowed.
+		memset(this->base_array, 0, (SIZE+1)*sizeof(int));
+		memset(this->base_array, 0, (SIZE+1)*sizeof(int));
 		this->n_allowed = 0;
 		this->value = 0;
 	}
@@ -94,9 +105,6 @@ struct cell_t {
 			}
 			return 1; // On success?
 		}
-		else{
-			this->value = val;
-		}
 		return -1;
 	}
 	int cell_destructor(cell_t* this) {
@@ -105,7 +113,41 @@ struct cell_t {
 	}
 	// WHAT WOULD HAVE BEEN A GORGEOUS cell_t struct :'(
 
-
+	/*
+		Serves the deep copy purpose when cell_t* definition was:
+		struct cell_t {
+			int row;
+			int col;
+			int* list;
+			int* base_array;
+			int n_allowed;
+			int value;	
+		}
+	 */
+	cell_t* deepcopy_board(cell_t* this) {
+		cell_t* other = (cell_t*)malloc(BOARD_SIZE*sizeof(cell_t));
+		// memset(other, 0, BOARD_SIZE*sizeof(cell_t));
+		int i = 1;
+		// print_board(this);
+		for(; i<BOARD_SIZE; ++i) {
+			// Copy the i'th
+			other[i].row = this[i].row;
+			other[i].col = this[i].col;
+			other[i].n_allowed = this[i].n_allowed;
+			other[i].value = this[i].value;
+			other[i].list = (int*)malloc((SIZE+1)*sizeof(int));
+			other[i].base_array = (int*)malloc((SIZE+1)*sizeof(int));
+			// memcpy((void*)(other[i].list), (void*)(this[i].list), (SIZE+1)*sizeof(int));
+			int j;
+			for(j=0; j<=SIZE; j++) {
+				(other[i].list)[j] = (this[i].list)[j];
+				(other[i].base_array)[j] = (this[i].base_array)[j];
+			}
+			// memcpy((void*)(other[i].base_array), (void*)(this[i].base_array), (SIZE+1)*sizeof(int));
+			
+		}
+		return other;
+	}
 struct pool_node_t {
 	cell_t* board_config; // malloc-ing
 	pool_node_t* next;
@@ -131,12 +173,13 @@ struct pool_t { // A doubly linked list. Allows for FIFO/LIFO.
 	pushf(x)
 	Return time state: [x,p,q,r,s...]
 	 */
-	void pushf(pool_t* this, cell_t* new_board) { // ASSERT: cell_t* is a fixed size board etc.
+	cell_t* pushf(pool_t* this, cell_t* new_board) { // ASSERT: cell_t* is a fixed size board etc.
 		struct pool_node_t* new_node = (pool_node_t*) malloc(sizeof(pool_node_t));
-		new_node->board_config = (cell_t*)malloc((BOARD_SIZE)*sizeof(cell_t));
-		memcpy((void*)new_node->board_config, (void*)new_board, (BOARD_SIZE)*sizeof(cell_t));
-		
+		// new_node->board_config = (cell_t*)malloc((BOARD_SIZE)*sizeof(cell_t));
+		// memcpy((void*)new_node->board_config, (void*)new_board, (BOARD_SIZE)*sizeof(cell_t));
+		new_node->board_config = deepcopy_board(new_board);
 		new_node->next = this->hd;
+		new_node->prev = NULL;
 		if(this->hd) { // this->hd is not NULL
 			this->hd->prev = new_node;
 		}
@@ -145,18 +188,20 @@ struct pool_t { // A doubly linked list. Allows for FIFO/LIFO.
 		if(this->sz == 1) {
 			this->tl = this->hd;
 		}
+		return new_node->board_config;
 	}
 	/*
 	Invocation time state : [...,p,q,r,s]
 	pushb(x)
 	Return time state: [...,p,q,r,s,x]
 	*/
-	void pushb(pool_t* this, cell_t* new_board) {
+	cell_t* pushb(pool_t* this, cell_t* new_board) {
 		struct pool_node_t* new_node = (pool_node_t*) malloc(sizeof(pool_node_t));
-		new_node->board_config = (cell_t*)malloc((BOARD_SIZE)*sizeof(cell_t));
-		memcpy((void*)new_node->board_config, (void*)new_board, (BOARD_SIZE)*sizeof(cell_t));
-
-		new_node->next = this->tl;
+		// new_node->board_config = (cell_t*)malloc((BOARD_SIZE)*sizeof(cell_t));
+		// memcpy((void*)new_node->board_config, (void*)new_board, (BOARD_SIZE)*sizeof(cell_t));
+		new_node->board_config = deepcopy_board(new_board);
+		new_node->next = NULL;
+		new_node->prev = this->tl;
 		if(this->tl) { // this->tl is not NULL
 			this->tl->next = new_node;
 		}
@@ -165,6 +210,7 @@ struct pool_t { // A doubly linked list. Allows for FIFO/LIFO.
 		if(this->sz == 1) {
 			this->hd = this->tl;
 		}
+		return new_node->board_config;
 	}
 	/*
 	Invocation time state : [p,q,r,s,...]
@@ -208,10 +254,84 @@ struct pool_t { // A doubly linked list. Allows for FIFO/LIFO.
 		return retval;
 	}
 	// END OF WHAT WOULD'VE BEEN AN AMAZING (dead)pool.
-/*
-	Convert a cell_t* board to an int** board
-	return: int** 
-*/
+	
+	// ugliest mf ever... returns -1 if number of possible branches < THRESHOLD_TO_BRANCH (macro defined.)
+	int branch(pool_t* this, cell_t* board, int max_pushes) {
+	// The following arrays will help me push into the workpool.
+		int * initialization_helper[SIZE+1]; // idx-i is the array (int*) of indices in board which have i allowed values
+		int  initialization_helper_sizes[SIZE+1]; // idx-i is the number of indices in board which have i allowed values
+		int mypushes = 0;
+		memset(initialization_helper_sizes, 0, (SIZE+1)*sizeof(int));
+		memset(initialization_helper, 0, (SIZE+1)*sizeof(int*));
+
+		// Iterate through board and fill up initialization_helper.
+		int idx_board;
+		for(idx_board=1; idx_board < BOARD_SIZE; ++idx_board) {
+			int nvalues = board[idx_board].n_allowed;
+			if( nvalues == 0 ) continue; // No values are allowed on this idx_board... ensures that initialization_helper[0] is empty.
+
+			if (initialization_helper_sizes[nvalues]==0) { // hasn't been allocated yet.
+				initialization_helper[nvalues] = (int*)malloc((BOARD_SIZE)*sizeof(int)); // This is 0 indexed.
+			}
+			initialization_helper[nvalues][initialization_helper_sizes[nvalues]] = idx_board;
+			initialization_helper_sizes[nvalues]++;
+		}
+		// Check if the number of possible branches is less than some value.
+		
+		int hma = 0; 
+		int count=0;
+		for( ;hma<=SIZE;++hma) {
+			count += hma* initialization_helper_sizes[hma];
+		}
+		if( count<THRESHOLD_FOR_BRANCH ) {
+			// Cleaning up after myself...
+			for(hma = 0; hma<=SIZE; ++hma) {
+				free(initialization_helper[hma]);
+			}
+			return -1; // Don't branch
+		}
+
+			// Iterates initialization_helper ... values between 2 and SIZE... Note: 0,1 are also included, but they're likely to be empty.
+		int idx_hma = 0; 
+			// Iterates initialization_helper[hma] ... values between 0 and initialization_helper_sizes[hma]
+		int idx_cell_list;
+		for(hma=0; hma<=SIZE; hma++) {
+			if( mypushes >= max_pushes ) break;
+			for( idx_hma=0; idx_hma< initialization_helper_sizes[hma]; idx_hma++) {
+				int branch_on = initialization_helper[hma][idx_hma];
+
+				for(idx_cell_list=1; idx_cell_list<=board[branch_on].n_allowed; ++idx_cell_list) {
+					// Some deep shit.
+					// critical if in 
+					cell_t* newboard;
+					if( omp_in_parallel() !=0 ) {
+						#pragma omp critical
+						{
+							newboard = pushf(this, board); // NO FRIGGING CLASS >.<	
+						}
+					} else {
+						newboard = pushf(this, board); // NO FRIGGING CLASS >.<		
+					}
+					
+					mypushes++;
+					set_value(&((newboard)[branch_on]), board[branch_on].list[idx_cell_list]);
+					print_board(newboard);
+				}
+			}
+		}
+
+		// Cleaning up after myself...
+		for(hma = 0; hma<=SIZE; ++hma) {
+			free(initialization_helper[hma]);
+		}
+		// make initialization helper etc.
+		return 0;
+	}
+
+	/*
+		Convert a cell_t* board to an int** board
+		return: int** 
+	*/
 	int** cell2board(cell_t* board){
 		int** new_board = malloc(SIZE * sizeof(int*));
 		int i;
@@ -225,41 +345,88 @@ struct pool_t { // A doubly linked list. Allows for FIFO/LIFO.
 		}
 		return new_board;
 	}
+
+	void print_cell(cell_t* cell) {
+		printf("Printing cell[%d,%d]=%d\n", cell->row, cell->col, cell->value);
+		printf("list:");
+		int i;
+		for (i=0; i<=SIZE; ++i) {
+			printf("%d ", cell->list[i]);
+		}
+		printf("\nbase_array:");
+		for (i=0; i<=SIZE; ++i) {
+			printf("%d ", cell->base_array[i]);
+		}
+		printf("\nn_allowed=%d\n", cell->n_allowed);
+	}
+	/*
+		print_board function launches a nuke. Obviously.
+	 */
+	void print_board(cell_t* board) {
+		int r,c;
+		printf("\n\t\tprinting board\n");
+		for(r=0; r<SIZE; r++) {
+			for(c=0; c<SIZE; c++) {
+				printf("%d ", board[r*SIZE + c + 1].value);
+			}
+			printf("\n");
+		}
+		printf("\n\n");
+	}
 /*
   Function call made from main reaches here
 */
 int** solveSudoku(int** originalGrid){
-  cell_t board[SIZE*SIZE + 1];
-  int r,c;
-  for(r = 0;r < SIZE;r++){
-	for(c = 0; c < SIZE; c++){
-	  int idx = r*SIZE + c + 1;
-	  cell_init(&board[idx]);
-	  if(originalGrid[r][c] != 0){
-	  	set_value( &board[idx], originalGrid[r][c] );
-	  }
-	  else{
-		int bitmask = (1<<(SIZE+1) ) - 2;
-		int jdx = 0;
-		int r_prime = r - (r % MINIGRIDSIZE);
-		int c_prime = c - (c % MINIGRIDSIZE);
-		for(;jdx<SIZE;jdx++){
-			bitmask &= ~(1<<originalGrid[r][jdx]);
-			bitmask &= ~(1<<originalGrid[jdx][c]);
-			bitmask &= ~(1<<originalGrid[r_prime + (jdx/MINIGRIDSIZE)][c_prime + (jdx % MINIGRIDSIZE)]);
-		}
-		jdx = 1;
-		for(;jdx<=SIZE;jdx++){
-			if(bitmask & 1<<jdx){
-				add_allowed(&board[idx],jdx);
+	cell_t board[BOARD_SIZE];
+	int r,c;
+	for(r = 0;r < SIZE;r++){
+		for(c = 0; c < SIZE; c++){
+			int idx = r*SIZE + c + 1;
+			cell_init(&board[idx]);
+			board[idx].row = r;
+			board[idx].col = c;
+			if(originalGrid[r][c] != 0){
+				set_value( &board[idx], originalGrid[r][c] );
+			} else {
+				int bitmask = (1<<(SIZE+1) ) - 2;
+				int jdx = 0;
+				int r_prime = r - (r % MINIGRIDSIZE);
+				int c_prime = c - (c % MINIGRIDSIZE);
+				for(;jdx<SIZE;jdx++){
+					bitmask &= ~(1<<originalGrid[r][jdx]);
+					bitmask &= ~(1<<originalGrid[jdx][c]);
+					bitmask &= ~(1<<originalGrid[r_prime + (jdx/MINIGRIDSIZE)][c_prime + (jdx % MINIGRIDSIZE)]);
+				}
+				jdx = 1;
+				for(;jdx<=SIZE;jdx++){
+					if(bitmask & 1<<jdx){
+						add_allowed(&board[idx],jdx);
+					}
+				}
 			}
 		}
-	  }
 	}
-  }
+	// board is the original problem.
 
-  int error = dfs(board);
-  return cell2board(board);
+	// Initialize pool_t
+	int error = PARTIAL_SOLN;
+	error = heuristic_solve(board); // Will become a shared variable soon. TODO : Uncomment.
+	if (error == SOLVED || error == NO_SOLN) {
+		return cell2board(board);
+	} // Simplify the problem... without parallelization man.
+
+
+	pool_t* workpool = (pool_t*) malloc(sizeof(pool_t)); // Really just three integers.
+	workpool->hd = NULL; workpool->tl = NULL; workpool->sz = 0;
+
+	branch( workpool, board, INITIAL_POOL_SIZE);
+	
+	#pragma omp parallel
+	{
+		// Worker thread.
+		while( )
+	}
+	return cell2board(board);
 
 }
 
@@ -284,6 +451,9 @@ int heuristic_solve(cell_t* board){
 					  	err_code = NO_SOLN;
 					  	return err_code;
 				  	}
+				  	// else{ // Don't need this block. Don't keep it.
+				  	// 	continue;
+				  	// }
 			  	}
 			  	else if(board[idx].n_allowed == 1){
 				  	flag = 1;
