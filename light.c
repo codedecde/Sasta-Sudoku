c966ad06a24abb69a6a780436a9e90e2cf8aea00
Haroun Habeeb <haroun7@gmail.com>
2016-04-09 00:41:41 +0530

    please be our saviour

--- light.c
+++ light.c
@@ -614,14 +614,18 @@ void undo_alterations(int* brd, int* nals, long* bits, int* alters, int nalters,
 	}
 	return;
 }
-
+#define THRESHOLD_LR (6*BOARD_SIZE/7)
 int hs(int* brd,int* nals,long* bits, int* base){
 	int fl_changed = 1, fl_unfilled = 0;
 	int idx,val,ctr;
 	int type;
 	int b,r,c,bidx,count,vidx,rp,cp;
 
+	// Start w/ lone ranger
+	int nunfilled = 0;
+
 	// Elimination
+	fl_changed = 1; // Should always run elimination.
 	while(fl_changed){
 		fl_changed = 0;
 		fl_unfilled = 0;
@@ -646,19 +650,19 @@ int hs(int* brd,int* nals,long* bits, int* base){
 
 			}
 			else{
+				nunfilled++;
 				fl_unfilled = 1;
 			}
 		}
 	}
+	
 	if(fl_unfilled == 0){
 		return SOLVED;
 	}
-	
-	while(fl_changed){
+	if( nunfilled > THRESHOLD_LR ) {
 		fl_changed = 0;
 		for(val = 1;val <= SIZE; ++val){
-		  //Row iterator
-		  
+		  //Row iterator 
 			for(r = 0; r < SIZE; ++r){
 				count = 0;
 				vidx = -1;
@@ -681,10 +685,7 @@ int hs(int* brd,int* nals,long* bits, int* base){
 				  set_board_value(brd,nals,bits,NULL,vidx,val);
 				}
 			}
-
 			//Column Iterator
-
-
 			for(c = 0; c < SIZE; ++c){
 			count = 0;
 			vidx = -1;
@@ -702,36 +703,35 @@ int hs(int* brd,int* nals,long* bits, int* base){
 			    }
 			  }
 			}
-			if(count == 1){
-			  fl_changed = 1;
-			  set_board_value(brd,nals,bits,NULL,vidx,val);
+				if(count == 1){
+				  fl_changed = 1;
+				  set_board_value(brd,nals,bits,NULL,vidx,val);
+				}
 			}
-			}
-
 			//BOX ITERATOR
 			for(r = 0; r < SIZE; ++r){
-			vidx = -1;
-			count = 0;
-			rp = (r/MINIGRIDSIZE) * MINIGRIDSIZE;
-			cp = (r%MINIGRIDSIZE) * MINIGRIDSIZE;
-			for(c = 0; c < SIZE; ++c){
-			  idx = TO_IDX((rp + (c/MINIGRIDSIZE)),(cp + (c%MINIGRIDSIZE)));
-			  if(brd[idx] == 0){
-			    if(nals[idx] == 0)
-			      return NO_SOLN;
-			    else if(brd[idx] && (1<<val)){
-			      count++;
-			      if(count == 2)
-			        break;
-			      else
-			        vidx = idx;
-			    }
-			  }
-			}
-			if(count == 1){
-			  fl_changed = 1;
-			  set_board_value(brd,nals,bits,NULL,vidx,val);
-			}
+				vidx = -1;
+				count = 0;
+				rp = (r/MINIGRIDSIZE) * MINIGRIDSIZE;
+				cp = (r%MINIGRIDSIZE) * MINIGRIDSIZE;
+				for(c = 0; c < SIZE; ++c){
+				  idx = TO_IDX((rp + (c/MINIGRIDSIZE)),(cp + (c%MINIGRIDSIZE)));
+				  if(brd[idx] == 0){
+				  	if(nals[idx] == 0)
+				      return NO_SOLN;
+				    else if(brd[idx] && (1<<val)){
+				      count++;
+				      if(count == 2)
+				        break;
+				      else
+				        vidx = idx;
+				    }
+				  }
+				}
+				if(count == 1){
+				    fl_changed = 1;
+					set_board_value(brd,nals,bits,NULL,vidx,val);
+				}
 			}
 		}
 	}
@@ -747,6 +747,7 @@ int hs(int* brd,int* nals,long* bits, int* base){
 			}
 		}
 	}
+	
 	return val;
 
 }
